e453209d8a3486cf9eaee95ef5eee0a28b1f90c4
William Derksen <wderksen@olin.edu>
2018-04-18 17:22:51 -0400

    remade project

---
+++ BCI Test Experiment/Assets/LSLIntMarkerStream.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using LSL;
+
+namespace Assets.LSL4Unity.Scripts
+{
+	[HelpURL("https://github.com/xfleckx/LSL4Unity/wiki#using-a-marker-stream")]
+	public class LSLIntMarkerStream : MonoBehaviour
+	{
+		private const string unique_source_id = "IntMarkerTestSource";
+
+		public string lslStreamName = "Unity_<Paradigma_Name_here>";
+		public string lslStreamType = "LSL_Marker_Strings";
+
+		private liblsl.StreamInfo lslStreamInfo;
+		private liblsl.StreamOutlet lslOutlet;
+		private int lslChannelCount = 1;
+
+		//Assuming that markers are never send in regular intervalls
+		private double nominal_srate = liblsl.IRREGULAR_RATE;
+
+		private const liblsl.channel_format_t lslChannelFormat = liblsl.channel_format_t.cf_int32;
+
+		private int[] sample;
+
+		void Awake()
+		{
+			sample = new int[lslChannelCount];
+
+			lslStreamInfo = new liblsl.StreamInfo(
+				lslStreamName,
+				lslStreamType,
+				lslChannelCount,
+				nominal_srate,
+				lslChannelFormat,
+				unique_source_id);
+
+			lslOutlet = new liblsl.StreamOutlet(lslStreamInfo);
+		}
+
+		public void Write(int marker)
+		{
+			sample[0] = marker;
+			lslOutlet.push_sample(sample);
+		}
+
+		public void Write(int marker, double customTimeStamp)
+		{
+			sample[0] = marker;
+			lslOutlet.push_sample(sample, customTimeStamp);
+		}
+
+		public void Write(int marker, float customTimeStamp)
+		{
+			sample[0] = marker;
+			lslOutlet.push_sample(sample, customTimeStamp);
+		}
+
+		public void WriteBeforeFrameIsDisplayed(int marker)
+		{
+			StartCoroutine(WriteMarkerAfterImageIsRendered(marker));
+		}
+
+		IEnumerator WriteMarkerAfterImageIsRendered(int pendingMarker)
+		{
+			yield return new WaitForEndOfFrame();
+
+			Write(pendingMarker);
+
+			yield return null;
+		}
+
+	}
+}
---
+++ BCI Test Experiment/Assets/SphereController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using Assets.LSL4Unity.Scripts;
+
+public class SphereController : MonoBehaviour {
+
+	public LSLIntMarkerStream markerStream;
+	public GameObject TestObject;
+
+	public float StateTime;
+	public float BlinkTime;
+	public float blinkChance;
+
+	private float StateClock = 0;
+	private float BlinkClock = 0;
+
+	private int State = 0;
+
+
+	// Use this for initialization
+	void Start () {
+		Assert.IsNotNull(markerStream, "You forgot to assign the reference to a marker stream implementation!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		StateClock += Time.deltaTime;
+
+		if (State == 2) {
+			BlinkClock += Time.deltaTime;
+			if (BlinkClock > BlinkTime) {
+				BlinkClock -= BlinkTime;
+				TestObject.SetActive (!TestObject.activeInHierarchy);
+			}
+		}
+
+		if (StateClock > StateTime) {
+			StateClock -= StateTime;
+			switch (State) {
+			case 0: //Off
+				State = Random.value < blinkChance ? 2 : 1;
+				TestObject.SetActive (true);
+				break;
+			case 1: //On
+				State = 0;
+				TestObject.SetActive (false);
+				break;
+			case 2: //Blink
+				State = 0;
+				TestObject.SetActive (false);
+				break;
+			}
+			markerStream.Write(State);
+		}
+	}
+}
